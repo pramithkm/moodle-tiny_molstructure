497c1867451c6b48b6243de08b42124762076e88
Céline Pervès <cperves@unistra.fr>
2024-03-25 14:05:32 +0100

    change version

--- version.php
+++ version.php
@@ -27,5 +27,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_molstructure';
-$plugin->release = '1.0.1';
-$plugin->version = 2024032200;
+$plugin->release = '1.0.2';
+$plugin->version = 2024032500;
